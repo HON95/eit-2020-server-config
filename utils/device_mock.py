42f3d3f19e3e569a4673290b4bd7c2d3d7d91403
Vemund Fredriksen <vemund.fredriksen@hotmail.com>
2020-03-06 21:13:13 +0100

    Added random temperature measurement

--- utils/device_mock.py
+++ utils/device_mock.py
@@ -4,6 +4,7 @@ class Device:
     def __init__(self, uuid):
         self.uuid = uuid
         self.pH = 7.0
+        self.temperature = random.randint(4, 17)
 
     def measurePH(self):
         self.pH += (random.randint(-1, 1) / 10)
@@ -14,8 +15,21 @@ class Device:
         
         return self.pH
 
+    def measureTemperature(self):
+        self.temperature += (random.randint(-20, 20) / 10)
+        if(self.temperature > 25):
+            self.temperature == 25
+        elif(self.temperature < 4):
+            self.temperature = 4
+        
+        return self.pH
+
     def json(self):
-        return "{" + "\"device\" : \"{uuid}\", \"ph\" : {ph}".format(uuid = self.uuid, ph=self.pH) + "}"
+        return "{" + "\"device\" : \"{uuid}\", \"ph\" : {ph}, \"temperature\" : {temp}".format(uuid = self.uuid, ph=self.pH, temp=self.temperature) + "}"
+
+
+if __name__ == "__main__":
+    uuids = ["8e313a89-309a-4e31-87c4-6de8fd386439", "c81a3fc1-7834-4386-b6da-eb27c455063f", "855da0b3-1b16-4055-b480-223a71942733", "6f62cebf-3adb-4665-98cc-83357ac76f91"]
 
 
 if __name__ == "__main__":
